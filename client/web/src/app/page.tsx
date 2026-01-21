1b23ce16528f8109fedab9d6f145f49e6b71fa42
vivek1240 <vivek12345singh@gmail.com>
2026-01-22 00:44:24 +0530

    Update mic selection color + animation tweak

--- client/web/src/app/page.tsx
+++ client/web/src/app/page.tsx
@@ -185,11 +185,10 @@ function ControlBar(props: {
             <motion.div
               initial={{ opacity: 0, top: "10px" }}
               animate={{ opacity: 1, top: 0 }}
-              exit={{ opacity: 0, top: "-10px" }}
+              exit={{ opacity: 0, top: "10px" }}
               transition={{
                 duration: 0.4,
                 ease: [0.09, 1.04, 0.245, 1.055],
-                delay: 0.25,
               }}
               className="flex h-8 absolute left-1/2 -translate-x-1/2  justify-center"
             >
