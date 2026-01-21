26f767087a96f9ef3584536a54c766c2a6ae4512
vivek1240 <vivek12345singh@gmail.com>
2026-01-22 00:44:24 +0530

    Swap out front for augmented simple front end

---
+++ client/web/src/app/page.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import { CloseIcon } from "@/components/CloseIcon";
+import {
+  AgentState,
+  DisconnectButton,
+  LiveKitRoom,
+  RoomAudioRenderer,
+  VoiceAssistantControlBar,
+  useMultibandTrackVolume,
+  useVoiceAssistant,
+} from "@livekit/components-react";
+import { useKrispNoiseFilter } from "@livekit/components-react/krisp";
+import { AnimatePresence, motion } from "framer-motion";
+import { MediaDeviceFailure } from "livekit-client";
+import { useCallback, useEffect, useState } from "react";
+import type { ConnectionDetails } from "./api/connection-details/route";
+import Image from "next/image";
+
+export default function Page() {
+  const [connectionDetails, updateConnectionDetails] = useState<
+    ConnectionDetails | undefined
+  >(undefined);
+  const [agentState, setAgentState] = useState<AgentState>("disconnected");
+
+  const onConnectButtonClicked = useCallback(async () => {
+    // Generate room connection details, including:
+    //   - A random Room name
+    //   - A random Participant name
+    //   - An Access Token to permit the participant to join the room
+    //   - The URL of the LiveKit server to connect to
+    //
+    // In real-world application, you would likely allow the user to specify their
+    // own participant name, and possibly to choose from existing rooms to join.
+
+    const url = new URL(
+      process.env.NEXT_PUBLIC_CONN_DETAILS_ENDPOINT ??
+        "/api/connection-details",
+      window.location.origin
+    );
+    const response = await fetch(url.toString());
+    const connectionDetailsData = await response.json();
+    updateConnectionDetails(connectionDetailsData);
+  }, []);
+
+  return (
+    <main data-lk-theme="default" className="h-full flex items-center">
+      <div className="w-full grid grid-rows-[64px_1fr_8px] lg:border border-white/20 h-full min-h-dvh lg:max-w-5xl mx-auto lg:min-h-[640px] lg:max-h-[640px] rounded-2xl px-4">
+        <header className="border-b border-white/20">
+          <div className="py-4 px-2 flex items-center justify-between">
+            <a href="https://groq.com" target="_blank">
+              <Image
+                width={122.667}
+                height={64}
+                src="/groq-logo.svg"
+                alt="Groq logo"
+                className="h-6 mt-1.5 w-auto"
+              />
+            </a>
+            <div>
+              Built with{" "}
+              <a
+                href="https://docs.livekit.io/agents"
+                className="pb-[1px] border-b border-white/40 hover:border-white/80 transition-all duration-75 ease-out"
+                target="_blank"
+              >
+                LiveKit
+              </a>
+            </div>
+          </div>
+        </header>
+        <LiveKitRoom
+          className="h-full flex flex-col gap-20 items-center justify-center bg-groq-accent-bg"
+          token={connectionDetails?.participantToken}
+          serverUrl={connectionDetails?.serverUrl}
+          connect={connectionDetails !== undefined}
+          audio={true}
+          video={false}
+          onMediaDeviceFailure={onDeviceFailure}
+          onDisconnected={() => {
+            updateConnectionDetails(undefined);
+          }}
+        >
+          <SimpleVoiceAssistant onStateChange={setAgentState} />
+          <ControlBar
+            onConnectButtonClicked={onConnectButtonClicked}
+            agentState={agentState}
+          />
+          <RoomAudioRenderer />
+        </LiveKitRoom>
+      </div>
+    </main>
+  );
+}
+
+function SimpleVoiceAssistant(props: {
+  onStateChange: (state: AgentState) => void;
+}) {
+  const { state, audioTrack } = useVoiceAssistant();
+  const volumes = useMultibandTrackVolume(audioTrack, { bands: 1 });
+
+  useEffect(() => {
+    props.onStateChange(state);
+  }, [props, state]);
+  return (
+    <div
+      className={`flex flex-col items-center pt-12 justify-center ${state === "disconnected" ? "opacity-10" : "opacity-100"} transition-opacity duration-300`}
+    >
+      <motion.div
+        animate={{
+          y: [0, -10, 0],
+        }}
+        transition={{
+          duration: 4,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      >
+        <div
+          className="w-48 h-48 rounded-full bg-white"
+          style={{
+            transform: `scale(${1 + volumes[0]})`,
+            boxShadow: `0 20px 30px 10px rgba(0, 0, 0, 0.1)`,
+          }}
+        ></div>
+      </motion.div>
+    </div>
+  );
+}
+
+function ControlBar(props: {
+  onConnectButtonClicked: () => void;
+  agentState: AgentState;
+}) {
+  /**
+   * Use Krisp background noise reduction when available.
+   * Note: This is only available on Scale plan, see {@link https://livekit.io/pricing | LiveKit Pricing} for more details.
+   */
+  const krisp = useKrispNoiseFilter();
+  useEffect(() => {
+    krisp.setNoiseFilterEnabled(true);
+  }, []);
+
+  return (
+    <div className="flex relative h-12">
+      <AnimatePresence>
+        {props.agentState === "disconnected" && (
+          <motion.button
+            initial={{ opacity: 0, top: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, top: "-10px" }}
+            transition={{ duration: 1, ease: [0.09, 1.04, 0.245, 1.055] }}
+            className="uppercase px-4 py-2 bg-transparent rounded-lg border border-white/20 hover:bg-white/10 transition-all duration-75 ease-out"
+            onClick={() => props.onConnectButtonClicked()}
+          >
+            <span className="text-white text-xs font-semibold tracking-widest">
+              Start a conversation
+            </span>
+          </motion.button>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {props.agentState !== "disconnected" &&
+          props.agentState !== "connecting" && (
+            <motion.div
+              initial={{ opacity: 0, top: "10px" }}
+              animate={{ opacity: 1, top: 0 }}
+              exit={{ opacity: 0, top: "-10px" }}
+              transition={{ duration: 0.4, ease: [0.09, 1.04, 0.245, 1.055] }}
+              className="flex h-8 absolute left-1/2 -translate-x-1/2  justify-center"
+            >
+              <VoiceAssistantControlBar controls={{ leave: false }} />
+              <DisconnectButton>
+                <CloseIcon />
+              </DisconnectButton>
+            </motion.div>
+          )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+function onDeviceFailure(error?: MediaDeviceFailure) {
+  console.error(error);
+  alert(
+    "Error acquiring camera or microphone permissions. Please make sure you grant the necessary permissions in your browser and reload the tab"
+  );
+}
--- client/web/tailwind.config.ts
+++ client/web/tailwind.config.ts
@@ -1,38 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const colors = require("tailwindcss/colors");
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-const cartesia = {
-  50: "#e0e8f8",
-  100: "#c2d1f1",
-  200: "#a4bbed",
-  300: "#86a4ea",
-  400: "#688ee8",
-  500: "#1d4ed8",
-  600: "#1a47c1",
-  700: "#163faa",
-  800: "#133793",
-  900: "#102f7c",
-  950: "#0c2565",
-};
 
 const groq = {
-  "accent-bg": "#F55036",
-  "content-bg": "#F55036",
-  "content-text": "#161519",
-  "accent-text": "#a1a0a7",
-  "action-text": "#F05A28",
-  "accent-border": "rgba(240, 90, 40, 0.15)",
-  "accent-text-active": "#f37f58",
-  "control-text": "#F55036",
-  "content-bg-darker": "#EAEAEB",
-  "button-bg": "#FFFFFF",
-  "button-text": "#E92A0C",
+  "accent-bg": "#434343",
 };
 
 const customColors = {
-  cartesia,
   groq,
 };
 
@@ -41,13 +15,6 @@ const config: Config = {
   content: ["./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -56,73 +23,15 @@ const config: Config = {
       gray: colors.neutral,
       ...colors,
       ...customColors,
-      border: "hsl(var(--border))",
-      input: "hsl(var(--input))",
-      ring: "hsl(var(--ring))",
-      background: "hsl(var(--background))",
-      foreground: "hsl(var(--foreground))",
-      primary: {
-        DEFAULT: "hsl(var(--primary))",
-        foreground: "hsl(var(--primary-foreground))",
-      },
-      secondary: {
-        DEFAULT: "hsl(var(--secondary))",
-        foreground: "hsl(var(--secondary-foreground))",
-      },
-      destructive: {
-        DEFAULT: "hsl(var(--destructive))",
-        foreground: "hsl(var(--destructive-foreground))",
-      },
-      muted: {
-        DEFAULT: "hsl(var(--muted))",
-        foreground: "hsl(var(--muted-foreground))",
-      },
-      accent: {
-        DEFAULT: "hsl(var(--accent))",
-        foreground: "hsl(var(--accent-foreground))",
-      },
-      popover: {
-        DEFAULT: "hsl(var(--popover))",
-        foreground: "hsl(var(--popover-foreground))",
-      },
-      card: {
-        DEFAULT: "hsl(var(--card))",
-        foreground: "hsl(var(--card-foreground))",
-      },
     },
     extend: {
-      boxShadow: {
-        "solid-offset": "3px 3px 0px 0px rgba(0, 0, 0, 1)",
-        "solid-offset-hover": "4px 4px 0px 0px rgba(0, 0, 0, 1)",
-        "solid-offset-active": "1px 1px 0px 0px rgba(0, 0, 0, 1)",
-        "solid-offset-accent": "3px 3px 0px 0px " + cartesia[500],
-        "solid-offset-accent-active": "3px 3px 0px 0px " + cartesia[500],
-        "solid-offset-destructive": "3px 3px 0px 0px " + colors.red[500],
-        "solid-offset-destructive-active": "1px 1px 0px 0px " + colors.red[500],
-        "solid-offset-destructive-hover": "4px 4px 0px 0px " + colors.red[500],
-      },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        md: "calc(var(--radius) - 2px)",
+        lg: "var(--radius)",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
